f6e14317a3a387137afb88177450d974e0d9be67
Tiago Garcia <tiagojsag@gmail.com>
2018-11-05 16:44:47 +0000

    Restore support for creating layers using a layer root object

--- app/src/app.js
+++ app/src/app.js
@@ -6,11 +6,9 @@ const config = require('config');
 const loader = require('loader');
 const ctRegisterMicroservice = require('ct-register-microservice-node');
 const ErrorSerializer = require('serializers/error.serializer');
+
 const mongoUri = process.env.MONGO_URI || `mongodb://${config.get('mongodb.host')}:${config.get('mongodb.port')}/${config.get('mongodb.database')}`;
 const koaValidate = require('koa-validate');
-// const FastlyPurge = require('fastly-purge');
-//
-// const fastlyPurge = new FastlyPurge(process.env.FASTLY_TOKEN);
 
 const koaBody = require('koa-body')({
     multipart: true,
@@ -21,7 +19,7 @@ const koaBody = require('koa-body')({
 
 let dbOptions = {};
 // KUBE CLUSTER
-if (mongoUri.indexOf('replicaSet') > - 1) {
+if (mongoUri.indexOf('replicaSet') > -1) {
     dbOptions = {
         db: { native_parser: true },
         replset: {
@@ -42,81 +40,73 @@ if (mongoUri.indexOf('replicaSet') > - 1) {
     };
 }
 
-const onDbReady = (err) => {
-
-    if (err) {
-        logger.error('MongoURI', mongoUri);
-        logger.error(err);
-        throw new Error(err);
-    }
-
-    const app = new Koa();
-
-    app.use(koaBody);
-
-    // app.use(async (ctx, next) => {
-    //     await next();
-    //     if (ctx.status === 200) {
-    //         if (ctx.method === 'GET' || ctx.method === 'HEAD') {
-    //             ctx.set('Surrogate-Key', 'layer');
-    //         } else {
-    //             fastlyPurge.key(process.env.FASTLY_SERVICE_ID, 'layer', (err, result) => {
-    //                 if (err) {
-    //                     logger.error('Error fastly', err);
-    //                 } else {
-    //                     logger.info('Fastly: ', result);
-    //                 }
-    //             });
-    //         }
-    //     }
-    // });
-
-    app.use(async (ctx, next) => {
-        try {
-            await next();
-        } catch (inErr) {
-            let error = inErr;
-            try {
-                error = JSON.parse(inErr);
-            } catch (e) {
-                logger.error('Parsing error');
-                error = inErr;
+async function init() {
+    return new Promise((resolve, reject) => {
+        async function onDbReady(err) {
+            if (err) {
+                logger.error('MongoURI', mongoUri);
+                logger.error(err);
+                reject(new Error(err));
             }
-            ctx.status = error.status || ctx.status || 500;
-            logger.error(error);
-            ctx.body = ErrorSerializer.serializeError(ctx.status, error.message);
-            if (process.env.NODE_ENV === 'prod' && ctx.status === 500) {
-                ctx.body = 'Unexpected error';
-            }
-            ctx.response.type = 'application/vnd.api+json';
+
+            const app = new Koa();
+
+            app.use(koaBody);
+
+            app.use(async (ctx, next) => {
+                try {
+                    await next();
+                } catch (inErr) {
+                    let error = inErr;
+                    try {
+                        error = JSON.parse(inErr);
+                    } catch (e) {
+                        logger.error('Parsing error');
+                        error = inErr;
+                    }
+                    ctx.status = error.status || ctx.status || 500;
+                    logger.error(error);
+                    ctx.body = ErrorSerializer.serializeError(ctx.status, error.message);
+                    if (process.env.NODE_ENV === 'prod' && ctx.status === 500) {
+                        ctx.body = 'Unexpected error';
+                    }
+                    ctx.response.type = 'application/vnd.api+json';
+                }
+            });
+
+            app.use(koaLogger());
+
+            koaValidate(app);
+
+            loader.loadRoutes(app);
+
+            const server = app.listen(process.env.PORT, () => {
+                ctRegisterMicroservice.register({
+                    info: require('../microservice/register.json'),
+                    swagger: require('../microservice/public-swagger.json'),
+                    mode: (process.env.CT_REGISTER_MODE && process.env.CT_REGISTER_MODE === 'auto') ? ctRegisterMicroservice.MODE_AUTOREGISTER : ctRegisterMicroservice.MODE_NORMAL,
+                    framework: ctRegisterMicroservice.KOA2,
+                    app,
+                    logger,
+                    name: config.get('service.name'),
+                    ctUrl: process.env.CT_URL,
+                    url: process.env.LOCAL_URL,
+                    token: process.env.CT_TOKEN,
+                    active: true
+                }).then(() => {
+                }, (error) => {
+                    logger.error(error);
+                    process.exit(1);
+                });
+            });
+            logger.info('Server started in ', process.env.PORT);
+            resolve({ app, server });
         }
+
+        logger.info(`Connecting to MongoDB URL ${mongoUri}`);
+        mongoose.connect(mongoUri, dbOptions, onDbReady);
     });
+}
 
-    app.use(koaLogger());
+module.exports = init;
 
-    koaValidate(app);
-
-    loader.loadRoutes(app);
-
-    app.listen(process.env.PORT, () => {
-        ctRegisterMicroservice.register({
-            info: require('../microservice/register.json'),
-            swagger: require('../microservice/public-swagger.json'),
-            mode: (process.env.CT_REGISTER_MODE && process.env.CT_REGISTER_MODE === 'auto') ? ctRegisterMicroservice.MODE_AUTOREGISTER : ctRegisterMicroservice.MODE_NORMAL,
-            framework: ctRegisterMicroservice.KOA2,
-            app,
-            logger,
-            name: config.get('service.name'),
-            ctUrl: process.env.CT_URL,
-            url: process.env.LOCAL_URL,
-            token: process.env.CT_TOKEN,
-            active: true
-        }).then(() => {}, (error) => {
-            logger.error(error);
-            process.exit(1);
-        });
-    });
-    logger.info('Server started in ', process.env.PORT);
-};
-
-mongoose.connect(mongoUri, dbOptions, onDbReady);
