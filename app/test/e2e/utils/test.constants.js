9fa34fb1697ff0834a14c52f9e77bea5d25118bd
stanislavgalin <stasgalin@gmail.com>
2019-10-03 10:53:16 +0300

    added possibility to filter list of layers by owner role

--- app/test/e2e/utils/test.constants.js
+++ app/test/e2e/utils/test.constants.js
@@ -18,7 +18,7 @@ const USERS = {
         }
     },
     MANAGER: {
-        id: '1a10d7c6e0a37126611fd7a7',
+        id: '1a10d7c6e0a37126611fd7a5',
         role: 'MANAGER',
         provider: 'local',
         email: 'user@control-tower.org',
@@ -36,8 +36,26 @@ const USERS = {
         }
     },
     ADMIN: {
-        id: '1a10d7c6e0a37126611fd7a7',
+        id: '1a10d7c6e0a37126611fd7a6',
         role: 'ADMIN',
+        provider: 'local',
+        email: 'user@control-tower.org',
+        name: 'test admin',
+        extraUserData: {
+            apps: [
+                'rw',
+                'gfw',
+                'gfw-climate',
+                'prep',
+                'aqueduct',
+                'forest-atlas',
+                'data4sdgs'
+            ]
+        }
+    },
+    SUPERADMIN: {
+        id: '1a10d7c6e0a37126611fd7a6',
+        role: 'SUPERADMIN',
         provider: 'local',
         email: 'user@control-tower.org',
         name: 'test admin',
