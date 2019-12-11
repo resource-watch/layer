4384ad6bee641b82316233c11ae24295f30449c0
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-11 12:07:38 +0000

    Ensure only admin users can sort by user name or role

--- app/test/e2e/layer-get-sort-user-fields.spec.js
+++ app/test/e2e/layer-get-sort-user-fields.spec.js
@@ -26,6 +26,33 @@ describe('GET layers sorted by user fields', () => {
         requester = await getTestServer();
     });
 
+    it('Getting layers sorted by user.role ASC without authentication should return 403 Forbidden', async () => {
+        await new Layer(createLayer()).save();
+
+        const response = await requester.get('/api/v1/layer').query({ sort: 'user.role' });
+        response.status.should.equal(403);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.be.equal('Sorting by user name or role not authorized.');
+    });
+
+    it('Getting layers sorted by user.role ASC with user with role USER should return 403 Forbidden', async () => {
+        await new Layer(createLayer()).save();
+
+        const response = await requester.get('/api/v1/layer').query({ sort: 'user.role', loggedUser: JSON.stringify(USER) });
+        response.status.should.equal(403);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.be.equal('Sorting by user name or role not authorized.');
+    });
+
+    it('Getting layers sorted by user.role ASC with user with role MANAGER should return 403 Forbidden', async () => {
+        await new Layer(createLayer()).save();
+
+        const response = await requester.get('/api/v1/layer').query({ sort: 'user.role', loggedUser: JSON.stringify(MANAGER) });
+        response.status.should.equal(403);
+        response.body.should.have.property('errors').and.be.an('array');
+        response.body.errors[0].should.have.property('detail').and.be.equal('Sorting by user name or role not authorized.');
+    });
+
     it('Getting layers sorted by user.role ASC should return a list of layers ordered by the role of the user who created the layer (happy case)', async () => {
         const layer1 = await new Layer(createLayer(null, null, null, USER.id)).save();
         const layer2 = await new Layer(createLayer(null, null, null, MANAGER.id)).save();
