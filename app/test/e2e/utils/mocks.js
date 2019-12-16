3d2dce2ceecf41da5cac7eb3aebdc1656e3bb439
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-16 16:41:28 +0000

    Minor refactors and improvements on tests

--- app/test/e2e/utils/mocks.js
+++ app/test/e2e/utils/mocks.js
@@ -12,18 +12,7 @@ const createMockUserRole = (role, userID) => nock(process.env.CT_URL)
     .get(`/auth/user/ids/${role}`)
     .reply(200, { data: [userID] });
 
-const mockUsersForSort = (users) => {
-    const betterUsers = users.map((u) => ({ ...u, _id: u.id }));
-
-    // Mock each user request (for includes=user)
-    betterUsers.map((user) => createMockUser([user]));
-
-    // Mock all users request (for sorting by user role)
-    createMockUser(betterUsers);
-};
-
 module.exports = {
     createMockUser,
-    createMockUserRole,
-    mockUsersForSort
+    createMockUserRole
 };
