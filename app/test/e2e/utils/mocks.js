9ba670afbec0969f204db5b7cc7881872892451b
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-16 15:51:00 +0000

    Add feature to sort layers according to user role and name

--- app/test/e2e/utils/mocks.js
+++ app/test/e2e/utils/mocks.js
@@ -12,7 +12,18 @@ const createMockUserRole = (role, userID) => nock(process.env.CT_URL)
     .get(`/auth/user/ids/${role}`)
     .reply(200, { data: [userID] });
 
+const mockUsersForSort = (users) => {
+    const betterUsers = users.map((u) => ({ ...u, _id: u.id }));
+
+    // Mock each user request (for includes=user)
+    betterUsers.map((user) => createMockUser([user]));
+
+    // Mock all users request (for sorting by user role)
+    createMockUser(betterUsers);
+};
+
 module.exports = {
     createMockUser,
-    createMockUserRole
+    createMockUserRole,
+    mockUsersForSort
 };
